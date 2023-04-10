260c8e9740d6034c005bee7f1992bc74a3cf333d
moaaz <moaz.elsayed002@gmail.com>
2023-04-10 17:51:55 +0200

    Add handler factory

---
+++ server/controllers/handlerFactory.js
@@ -0,0 +1,72 @@
+const { query } = require('express')
+const AppError = require('../utils/appError')
+const catchAsync = require('../utils/catchAsync')
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByPk(req.params.id)
+    if (!doc) {
+      return next(new AppError('no document found with that id', 404))
+    }
+
+    await doc.update(req.body)
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    })
+  })
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByPk(req.params.id)
+    if (!doc) {
+      return next(new AppError('no document found with that id', 404))
+    }
+
+    await doc.destroy()
+    res.status(204).json({
+      status: 'success',
+    })
+  })
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, nex) => {
+    const doc = await Model.create(req.body)
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    })
+  })
+
+exports.getOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByPk(req.params.id)
+    if (!doc) {
+      return next(new AppError('no tour found with that id', 404))
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    })
+  })
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findAll()
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      requestedAt: req.requestedAt,
+      data: {
+        data: doc,
+      },
+    })
+  })
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,66 +1,13 @@
 const path = require('path')
 const User = require(path.join(__dirname, '..', 'models', 'user'))
-const catchAsync = require(path.join(__dirname, '..', 'utils', 'catchAsync'))
+const Factory = require(path.join(__dirname, 'handlerFactory'))
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.findAll()
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: users,
-  })
-})
+exports.getAllUsers = Factory.getAll(User)
 
-exports.createUser = catchAsync(async (req, res) => {
-  const { username, email, password } = req.body
+exports.createUser = Factory.createOne(User)
 
-  const user = await User.create({
-    username,
-    email,
-    password,
-  })
+exports.getUserById = Factory.getOne(User)
 
-  res.status(201).json({
-    status: 'success',
-    data: user,
-  })
-})
+exports.updateUser = Factory.updateOne(User)
 
-exports.getUserById = catchAsync(async (req, res) => {
-  const user = await User.findByPk(req.params.id)
-
-  res.status(200).json({
-    status: 'success',
-    data: user,
-  })
-})
-
-exports.updateUser = catchAsync(async (req, res) => {
-  const user = await User.findByPk(req.params.id)
-  if (!user) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'User not found',
-    })
-  }
-  console.log(user)
-  await user.update(req.body)
-  res.status(200).json({
-    status: 'success',
-    data: user,
-  })
-})
-
-exports.deleteUser = catchAsync(async (req, res) => {
-  const user = await User.findByPk(req.params.id)
-  if (!user) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'User not found',
-    })
-  }
-  await user.destroy()
-  res.status(204).json({
-    status: 'success',
-  })
-})
+exports.deleteUser = Factory.deleteOne(User)
