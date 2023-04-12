4b49bfda47dea185a6d2c418412bebf3373b28ec
moaaz <moaz.elsayed002@gmail.com>
2023-04-12 15:24:48 +0200

    Add comments routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const userRouter = require(path.join(__dirname, 'routes', 'userRoutes'))
 const taskRouter = require(path.join(__dirname, 'routes', 'taskRoutes'))
 const topicRouter = require(path.join(__dirname, 'routes', 'topicRoutes'))
 const boardRouter = require(path.join(__dirname, 'routes', 'boardRoutes'))
+const taskCommentRouter = require(path.join(
+  __dirname,
+  'routes',
+  'taskCommentRoutes'
+))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,6 +20,7 @@ app.use('/api/v1/users', userRouter)
 app.use('/api/v1/tasks', taskRouter)
 app.use('/api/v1/topics', topicRouter)
 app.use('/api/v1/boards', boardRouter)
+app.use('/api/v1/task-comments', taskCommentRouter)
 
 // routes
 app.get('/', (req, res) => {
---
+++ server/routes/taskCommentRoutes.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const taskCommentController = require(path.join(
+  __dirname,
+  '..',
+  'controllers',
+  'taskCommentController'
+))
+
+const router = express.Router()
+
+router.get('/', taskCommentController.getAllTaskComments)
+
+router.get('/:id', taskCommentController.getTaskCommentById)
+
+router.post('/', taskCommentController.createTaskComment)
+
+router.put('/:id', taskCommentController.updateTaskComment)
+
+router.delete('/:id', taskCommentController.deleteTaskComment)
+
+module.exports = router
