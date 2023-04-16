bd4ac6ccbbccc3e6aee463c322399217d4dbca7e
Hazem <hazem31009444@f-eng.tanta.edu.eg>
2023-04-16 15:58:47 +0200

    Adding Tasks cards styles WIP1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                btn: '0px 1px 3px rgba(47, 43, 67, 0.1), inset 0px -1px 0px rgba(47, 43, 67, 0.1)',
+                card: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+            },
+        },
     },
     plugins: [],
 }
