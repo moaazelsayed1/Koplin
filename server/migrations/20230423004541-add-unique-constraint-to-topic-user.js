6724a6db38281bc116ac8c3cd0a5fe00a2606374
moaaz <moaz.elsayed002@gmail.com>
2023-04-23 02:54:45 +0200

    Add unique constraint to `topicUser` table
    Prevents adding user to the same topic multiple times.

---
+++ server/migrations/20230423004541-add-unique-constraint-to-topic-user.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addConstraint('topic_user', {
+      type: 'unique',
+      fields: ['topic_id', 'user_id'],
+      name: 'topic_user_topic_id_user_id_key',
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeConstraint(
+      'topic_user',
+      'topic_user_topic_id_user_id_key'
+    )
+  },
+}
