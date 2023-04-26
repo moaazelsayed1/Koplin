82cb290cbc41ed0f591df8b2f83a14df477357df
moaaz <moaz.elsayed002@gmail.com>
2023-04-26 04:32:36 +0200

    Update `boardRoutes`

--- server/controllers/boardController.js
+++ server/controllers/boardController.js
@@ -1,6 +1,29 @@
 const path = require('path')
 const Board = require(path.join(__dirname, '..', 'models', 'board'))
 const Factory = require(path.join(__dirname, 'handlerFactory'))
+const Topic = require(path.join(__dirname, '..', 'models', 'topic'))
+const AppError = require(path.join(__dirname, '..', 'utils', 'appError'))
+const catchAsync = require(path.join(__dirname, '..', 'utils', 'catchAsync'))
+
+exports.getAllBoardsByTopic = catchAsync(async (req, res, next) => {
+  const { topicId } = req.params
+
+  const topic = await Topic.findByPk(topicId)
+
+  if (!topic) {
+    return next(new AppError('No topic found with that ID', 404))
+  }
+
+  const boards = await Board.findAll({ where: { topic_id: topicId } })
+
+  res.status(200).json({
+    status: 'success',
+    results: boards.length,
+    data: {
+      boards,
+    },
+  })
+})
 
 exports.getAllBoards = Factory.getAll(Board)
 
--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -7,15 +7,38 @@ const boardController = require(path.join(
   'boardController'
 ))
 
+const authController = require(path.join(
+  __dirname,
+  '..',
+  'controllers',
+  'authController'
+))
+
+const topicUserController = require(path.join(
+  __dirname,
+  '..',
+  'controllers',
+  'topicUserController'
+))
+
 const router = express.Router()
 
+// unused
 router.get('/', boardController.getAllBoards)
-router.get('/:id', boardController.getBoardById)
 
-router.post('/', boardController.createBoard)
+router.use(authController.protect)
 
-router.put('/:id', boardController.updateBoard)
+router
+  .route('/topic/:topicId')
+  .get(topicUserController.setTopicUserId, boardController.getAllBoardsByTopic)
+  .post(topicUserController.setTopicUserId, boardController.createBoard)
 
-router.delete('/:id', boardController.deleteBoard)
+router
+  .route('/topic/:topicId/board/:id')
+  .get(topicUserController.setTopicUserId, boardController.getBoardById)
+  .patch(topicUserController.setTopicUserId, boardController.updateBoard)
+  .delete(topicUserController.setTopicUserId, boardController.deleteBoard)
+
+// TODO => all boards of a user (maybe later)
 
 module.exports = router
