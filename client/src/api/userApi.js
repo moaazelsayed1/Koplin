d86ece2ba4f0bc5d3a2ba305a663c153155c0115
Hazem <hazem31009444@f-eng.tanta.edu.eg>
2023-04-29 04:52:44 +0300

    Add users fixed

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -4,6 +4,7 @@ const UserApi = {
     getOne: (username) => axiosClient.get(`users/${username}`),
     getOnebyId: (id) => axiosClient.get(`users/${id}`),
     LogOut: () => axiosClient.get(`users/logout`),
+    update: (id, params) => axiosClient.patch(`users/${id}`, params),
 }
 
 export default UserApi
