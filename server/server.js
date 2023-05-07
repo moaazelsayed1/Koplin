110e16a6585cef367029be3ada8df726f977cb16
moaaz <moaz.elsayed002@gmail.com>
2023-05-07 17:32:16 +0300

    Attach the socket.io instance to the http server

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = require('./index')
 const path = require('path')
 const io = require('./socket')
+const http = require('http')
 
 const sequelize = require(path.join(__dirname, 'utils', 'database'))
 
 const port = process.env.PORT || 3000
+const server = http.createServer(app)
+io.attach(server)
 
 sequelize
   .sync()
   .then(() => console.log('tables created successfully'))
   .catch((error) => console.log('Error creating User table:', error))
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server listening on port ' + port)
 })
