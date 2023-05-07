a0bc6ae6c99e1a00f5da042a0ff446bdcd8570c4
moaaz <moaz.elsayed002@gmail.com>
2023-05-07 14:02:31 +0300

    Add notifications controller

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -8,3 +8,11 @@ const Notification = require(path.join(
 const Factory = require(path.join(__dirname, 'handlerFactory'))
 
 exports.getAllNotifications = Factory.getAll(Notification)
+
+exports.createNotification = Factory.createOne(Notification)
+
+exports.getNotificationById = Factory.getOne(Notification)
+
+exports.updateNotification = Factory.updateOne(Notification)
+
+exports.deleteNotification = Factory.deleteOne(Notification)
--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -11,4 +11,12 @@ const router = express.Router()
 
 router.get('/', notificationController.getAllNotifications)
 
+router.get('/:id', notificationController.getNotificationById)
+
+router.post('/', notificationController.createNotification)
+
+router.patch('/:id', notificationController.updateNotification)
+
+router.delete('/:id', notificationController.deleteNotification)
+
 module.exports = router
