3c6610ccfadfe69438c64bf66a8766d7e5a09e11
moaaz <moaz.elsayed002@gmail.com>
2023-05-15 12:40:17 +0300

    Increase each IP to 200 requests per `windowMs`

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 200, // limit each IP to 100 requests per windowMs
 })
 app.use(limiter)
 
