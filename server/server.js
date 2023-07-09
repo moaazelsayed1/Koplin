f77ed25039b9b1a09d1642e0830089634f01c7b4
moaaz <moaz.elsayed002@gmail.com>
2023-07-09 12:09:52 +0300

    Remove sync sequelize

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ const port = process.env.PORT || 3000
 const server = http.createServer(app)
 io.attach(server)
 
-sequelize
-  .sync()
-  .then(() => console.log('tables created successfully'))
-  .catch((error) => console.log('Error creating User table:', error))
+/* sequelize */
+/*   .sync() */
+/*   .then(() => console.log('tables created successfully')) */
+/*   .catch((error) => console.log('Error creating User table:', error)) */
 
 server.listen(port, () => {
   console.log('Server listening on port ' + port)
