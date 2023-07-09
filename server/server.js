158321580a91135a6efaf9ac9b495769f76419b1
moaaz <moaz.elsayed002@gmail.com>
2023-07-09 12:14:49 +0300

    Add `createTable` manually

--- server/server.js
+++ server/server.js
@@ -15,6 +15,24 @@ io.attach(server)
 /*   .then(() => console.log('tables created successfully')) */
 /*   .catch((error) => console.log('Error creating User table:', error)) */
 
+async function createTables() {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection to the database has been established successfully.')
+
+    await sequelize.query('CREATE TABLE IF NOT EXISTS Users (...)', {
+      type: sequelize.QueryTypes.RAW,
+    })
+    console.log('Tables created successfully.')
+  } catch (error) {
+    console.error('Error creating User table:', error)
+  } finally {
+    await sequelize.close()
+  }
+}
+
+createTables()
+
 server.listen(port, () => {
   console.log('Server listening on port ' + port)
 })
